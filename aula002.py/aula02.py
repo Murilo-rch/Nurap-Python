dfef3f08e3da579bd636c1491495647c12c3e401
Murilo-rch <murilo.rocha2904@gmail.com>
2024-12-03 10:03:34 -0300

    Add files via upload

---
+++ aula002.py/aula02.py
@@ -0,0 +1,11 @@
+print("Bom Dia brabos")
+
+nome_usuario = "Louco da Coberta" # tipo -> str = stringe
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.81 # tioi -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print("seja bem vindo", nome_usuario)
+print("sua idade é", idade_usuario, "anos")
+print("sua altura é", altura_usuario, "metros")
+print("você é maior de idade?", maior_idade)
